a4b407de6b98ec28f09f0411e0cdb3397632ae3d
Zee <zachzdepski@gmail.com>
2021-12-07 19:16:25 -0500

    Unzipped movement file to re-add to the repository

--- SnakeBoard2/GameBoard.vb
+++ SnakeBoard2/GameBoard.vb
@@ -6,6 +6,7 @@
     Dim Generator As System.Random = New System.Random()
     Dim direction As Integer = 0
     Dim change As Boolean = False
+    Dim exitGame As Boolean = False
 
     Dim vert As Integer = 197 'Snake head location vertial
     Dim hor As Integer = 375   'Snake head location Horizontal
@@ -17,6 +18,7 @@
         pb.BackColor = Color.Red
         pb.Visible = True
         Me.Controls.Add(pb)
+        Timer1.Start()
 
         'Dim menu As New StartMenu
         'menu.Show()
@@ -24,13 +26,12 @@
 
     End Sub
 
+
+
     Private Sub moveApple()
         pb.Location = New Point(Generator.Next(MyMin, MyMaxX), Generator.Next(MyMin, MyMaxY))
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs)
-        moveApple()
-    End Sub
 
     Public Sub eat()
 
@@ -62,64 +63,48 @@
     End Sub
     Private Sub moveSnake(ByVal sender As System.Object, ByVal e As KeyEventArgs) Handles MyBase.KeyDown
         eat()
+        change = False
         Select Case e.KeyCode
             Case Keys.Right
-                hor = hor + 5
-                pctSnake.Location = New Point(hor, vert)
-                direction = 1
-
+                While change = False
+                    My.Application.DoEvents()
+                    hor = hor + 5
+                    pctSnake.Location = New Point(hor, vert)
+                    checkColl()
+                    System.Threading.Thread.Sleep(80)
+                End While
                 checkColl()
             Case Keys.Left
-                hor = hor - 5
-                pctSnake.Location = New Point(hor, vert)
-                direction = 2
-
-                checkColl()
-            Case Keys.Up
-                vert = vert - 5
-                pctSnake.Location = New Point(hor, vert)
-                direction = 3
-
-                checkColl()
-            Case Keys.Down
-                vert = vert + 5
-                pctSnake.Location = New Point(hor, vert)
-                direction = 4
-                checkColl()
-        End Select
-        Select Case direction
-            Case 1
                 While change = False
-                    hor = hor + 5
-                    If e.KeyCode = Keys.Up Or e.KeyCode = Keys.Right Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Left Then
-                        change = True
-                    End If
-                End While
-            Case 2
-                While change = False
+                    My.Application.DoEvents()
                     hor = hor - 5
-                    If e.KeyCode = Keys.Up Or e.KeyCode = Keys.Right Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Left Then
-                        change = True
-                    End If
+                    pctSnake.Location = New Point(hor, vert)
+                    checkColl()
+                    System.Threading.Thread.Sleep(80)
                 End While
-            Case 3
+            Case Keys.Up
                 While change = False
-                    vert = vert + 5
-                    If e.KeyCode = Keys.Up Or e.KeyCode = Keys.Right Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Left Then
-                        change = True
-                    End If
-                End While
-            Case 4
-                While change = False
+                    My.Application.DoEvents()
                     vert = vert - 5
-                    If e.KeyCode = Keys.Up Or e.KeyCode = Keys.Right Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Left Then
-                        change = True
-                    End If
+                    pctSnake.Location = New Point(hor, vert)
+                    checkColl()
+                    System.Threading.Thread.Sleep(80)
+                End While
+            Case Keys.Down
+                While change = False
+                    My.Application.DoEvents()
+                    vert = vert + 5
+                    pctSnake.Location = New Point(hor, vert)
+                    checkColl()
+                    System.Threading.Thread.Sleep(80)
                 End While
         End Select
+
+        If e.KeyCode = Keys.Up Or e.KeyCode = Keys.Right Or e.KeyCode = Keys.Down Or e.KeyCode = Keys.Left Then
+            change = True
+        End If
+
     End Sub
-
-
 
     Private Sub checkColl()
         If pctSnake.Location.X > 760 Or pctSnake.Location.X < 10 Then
@@ -127,5 +112,9 @@
         ElseIf pctSnake.Location.Y > 550 Or pctSnake.Location.Y < 10 Then
             MessageBox.Show("Collision")
         End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs)
+        exitGame = True
     End Sub
 End Class
